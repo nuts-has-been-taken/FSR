d59b2c2e66ac967ce54502cd7b6d5ac2b8d8046f
carbarcha <samhoward13261@gmail.com>
2024-02-27 18:17:14 +0800

    test notion

--- utils/deepface.py
+++ utils/deepface.py
@@ -1,4 +1,4 @@
 from deepface import DeepFace
 
 def get_face_vector(img_path):
-    return DeepFace.represent(img_path=img_path, model_name="ArcFace")[0]["embedding"]
+    return DeepFace.represent(img_path=img_path, model_name="ArcFace")[0]["embedding"] 
